ee98e0ad0f67faf0d9e5684691c06ed9bff7eaca
Sam Yang <sam9883043@gmail.com>
2025-04-04 22:14:39 +0900

    feat: add checkstyle and spotless plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.4.4"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("checkstyle")
+	id("com.diffplug.spotless") version "6.25.0"
 }
 
 group = "com.example"
@@ -26,4 +28,20 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+checkstyle {
+	toolVersion = "10.12.7"
+	configFile = file("config/checkstyle/checkstyle.xml")
+}
+
+spotless {
+	java {
+		// 使用 Google Java 格式
+		googleJavaFormat()
+		// 移除未使用的 imports
+		removeUnusedImports()
+		// 確保文件以新行結束
+		endWithNewline()
+	}
 }
--- src/main/java/com/example/demo/controller/TestFormatController.java
+++ src/main/java/com/example/demo/controller/TestFormatController.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
-
-  @GetMapping("/hello")
-  public String hello() {
-    return "Hello World!";
+public class TestFormatController {
+  @GetMapping("/test")
+  public String test() {
+    String message = "This is a test";
+    return message;
   }
 }
